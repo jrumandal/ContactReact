49ee1bf85d1a7d88c7f60fd8b8bbf1d5e44a5e35
John Ralph <jrumandal@gmail.com>
2019-06-23 22:25:58 +0100

    Added modal example inside ButtonNewContact component

--- src/components/ButtonNewContact/ButtonNewContact.jsx
+++ src/components/ButtonNewContact/ButtonNewContact.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './ButtonNewContact.css';
+import FormNewContact from '../FormNewContact/FormNewContact';
+import './ButtonNewContact.css';
 
 class ButtonNewContact extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+    };
+
+    handleOpening = (open) => () => {
+        this.setState({
+            isOpen: open,
+        });
+    }
     render () {
         return (
-            <div className="btnNew">
-                <ion-icon name="ios-add-circle-outline"></ion-icon>
-            </div>
+            // potevo usare il div ma non sapevo se c'erano degli stili che sarebbero andati in conflitto
+            <React.Fragment>
+                <div className="btnNew" onClick={this.handleOpening(true)}>
+                    <ion-icon name="ios-add-circle-outline"></ion-icon>
+                </div>
+
+                <div className={`windowFluttuante ${this.state.isOpen ? 'open': ''}`}>
+                    <FormNewContact />
+
+                    <button onClick={this.handleOpening(false)}>Close</button>
+                </div>
+            </React.Fragment>
+            
         );
     }
 }
